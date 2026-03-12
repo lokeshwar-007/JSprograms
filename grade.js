768d06c9b564e4d2422a7ef0e586248842c9fd63
lokeshwar <lokeshwar18534@gmail.com>
2026-03-12 10:50:48 +0530

    saved grade.js

---
+++ grade.js
@@ -0,0 +1,9 @@
+console.log("Providing Grades To Students")
+let marks = 45
+if (marks >= 90){
+    console.log("U got A grade")
+}else if(marks >= 80){
+    console.log("you got B grade")
+}else{
+    console.log("you got C grade")
+}
