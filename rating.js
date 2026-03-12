04230ee3f477dd698e001ec438f0b9bc2687f0f2
lokeshwar <lokeshwar18534@gmail.com>
2026-03-12 11:25:30 +0530

    rating.js added

---
+++ rating.js
@@ -0,0 +1,13 @@
+console.log("RATING....")
+let rating = 3
+switch(rating){
+    case 1:
+        console.log("Poor")
+        break
+    case 2:
+        console.log("Average")
+        break
+    case 3:
+        console.log("Gooooood")
+        break
+}
