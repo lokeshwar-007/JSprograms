10999a08ccca5431d36853544bb6240296570e3f
lokeshwar <lokeshwar18534@gmail.com>
2026-03-12 11:15:19 +0530

    added monthday.js

---
+++ monthday.js
@@ -0,0 +1,40 @@
+console.log("weekday number")
+let mday = 12
+switch(mday){
+    case 1:
+        console.log("January")
+        break
+    case 2:
+        console.log("Febraury")
+        break
+    case 3:
+        console.log("March")
+        break
+    case 4:
+        console.log("April")
+        break
+    case 5:
+        console.log("May")
+        break
+    case 6:
+        console.log("June")
+        break
+    case 7:
+        console.log("July")
+        break
+    case 8:
+        console.log("August")
+        break
+    case 9:
+        console.log("September")
+        break
+    case 10:
+        console.log("October")
+        break
+    case 11:
+        console.log("November")
+        break
+    case 12:
+        console.log("December")
+        break
+}
