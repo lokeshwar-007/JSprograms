b74c4d4a1111ef79fb1a90d3d35c5f4f425f1a91
lokeshwar <lokeshwar18534@gmail.com>
2026-03-13 10:07:57 +0530

    saved the program

---
+++ evenOddChecker.js
@@ -0,0 +1,9 @@
+console.log("EVEN/ODD CHECKER BY FUNCTIONS")
+function checker(num){
+    if (num % 2 == 0){
+        console.log(num," is even number!!!")
+    }else{
+        console.log(num," is odd number!!!")
+    }
+}
+checker(234)
